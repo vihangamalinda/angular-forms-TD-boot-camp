80c0cb1f048b64a628619a4c5d63a151ba11b60c
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-10 05:11:23 -0800

    feat:Setting the suggested username using ngForm patchValue()

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -4,27 +4,40 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild('f') signupForm:NgForm;
+  @ViewChild('f') signupForm: NgForm;
 
   defaultQuestion = 'pet';
-  answer='';
-  genders=['male','female'];
+  answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData:{
+    //     username:suggestedName,
+    //     email:''
+    //   },
+    //   secret:'pet',
+    //   questionAnswer:'',
+    //   gender:'male'
+    // })
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form:NgForm){
   //   console.log(`Vihngaaaaaaaaaaaaaaaa`,form)
   // }
 
-  onSubmit(){
-    console.log(`Vihngaaaaaaaaaaaaaaaa`,this.signupForm.valid);
-    console.log(`this.signupForm`,this.signupForm);
-
+  onSubmit() {
+    console.log(`Vihngaaaaaaaaaaaaaaaa`, this.signupForm.valid);
+    console.log(`this.signupForm`, this.signupForm);
   }
 
   // isFormInvalid(){
